5dc02f6b27eb052111e363a41361c8844fcf7e08
Alperen Yurtseven <31616211+alpyurtseven@users.noreply.github.com>
2022-06-06 19:59:59 +0300

    up up

--- ArduinoConsole/Program.cs
+++ ArduinoConsole/Program.cs
@@ -2,32 +2,119 @@
 using System.Collections.Generic;
 using System.IO.Ports;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using Data.Models;
+using Newtonsoft.Json;
+
 
 namespace ArduinoConsole
 {
     internal class Program
     {
         static SerialPort _serialPort;
+        private static readonly HttpClient client = new HttpClient();
+        enum Level
+        {
+            Low,
+            Medium,
+            High
+        }
+
+        private static Answer answer;
         static void Main(string[] args)
         {
             _serialPort = new SerialPort();
-            _serialPort.PortName = "COM5";//Set your board COM
+            _serialPort.PortName = "COM3";
             _serialPort.BaudRate = 9600;
-        
-            foreach (var st in SerialPort.GetPortNames())
+            _serialPort.Open();
+
+            bool potantiometerState = false;
+
+            while (_serialPort.IsOpen)
             {
-                Console.WriteLine(st);
+                string a = _serialPort.ReadExisting();
+                if (a != "")
+                {
+                    if (a == "CST0" || a == "CST1")
+                    {
+                        getPotantiometerTime(a);
+                    }else if(a == "CGV")
+                    {
+                        getPotantiometerValue(a);
+                    }else if (a == "CBV")
+                    {
+                        getButtonAnswer(a);
+                    }
+
+
+
+                }
+                Thread.Sleep(200);
+            }
+        }
+
+        private static void getPotantiometerTime(string command)
+        {
+            if (command == "CST0")
+            {
+
+            }else if (command == "CST1")
+            {
+
+            }
+        }
+
+        private static void getPotantiometerValue(string command)
+        {
+            if (command == "CGV")
+            {
+
+            }
+        }
+
+        private static void getButtonAnswer(string command)
+        {
+            if (command == "CBV")
+            {
+
+            }
+        }
+
+        private static void updateOrCreateAnswer(string text)
+        {   
+            if (answer == null)
+            {
+                answer = new Answer();
             }
 
-            //while (true)
-            //{
-            //    string a = _serialPort.ReadExisting();
-            //    Console.WriteLine(a);
-            //    Thread.Sleep(200);
-            //}
+            new JavaScriptSerializer().Serialize(text);
+
+            answer.AnswerText = "";
+        }
+
+        private static void confirmAnswer()
+        {
+            throw new ArgumentException();
+        }
+
+        private static async Task sendAnswer(Answer answer)
+        {
+            var content = JsonConvert.SerializeObject(answer);
+            var buffer = System.Text.Encoding.UTF8.GetBytes(content);
+            var byteContent = new ByteArrayContent(buffer);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var stringTask = client.PostAsync("https://api.github.com/orgs/dotnet/repos",byteContent);
+
+            var msg = await stringTask;
+            Console.Write(msg);
         }
     }
 }
